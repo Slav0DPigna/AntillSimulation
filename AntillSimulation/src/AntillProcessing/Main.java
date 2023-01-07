a52b557866fa028fd833c2f11e786282460e00a5
slav0 <Salvatorepignataro0@gmail.com>
2023-01-07 10:42:33 +0100

    Add goBack function

--- AntillSimulation/src/AntillProcessing/Main.java
+++ AntillSimulation/src/AntillProcessing/Main.java
@@ -5,13 +5,13 @@ import processing.core.PApplet;
 import java.util.ArrayList;
 
 public class Main extends PApplet {
-    protected Ant[] ants=new Ant[20];
+    protected Ant[] ants=new Ant[1];
     protected ArrayList<Food> foods=new ArrayList<>();
 
 
     @Override
     public void setup() {
-        for(int i=0;i<20;i++)
+        for(int i=0;i<1;i++)
             ants[i]=new Ant();
         frameRate(120);
     }//setup
@@ -24,22 +24,26 @@ public class Main extends PApplet {
     @Override
     public void draw() {
         background(255,255,255);
-        if(mousePressed && key=='n'){
-            foods.add(new Food(new Position(mouseX,mouseY),50));
-            key=' ';
-            for(int i=0;i<foods.size();i++)
+        if (mousePressed && key == 'n') {
+            foods.add(new Food(new Position(mouseX, mouseY), 50));
+            key = ' ';
+            for (int i = 0; i < foods.size(); i++)
                 println(foods.get(i));
         }
-        for(int i=0;i<foods.size();i++){
-            fill(255,255,0);
-            circle(foods.get(i).getPos().getX(),foods.get(i).getPos().getY(),foods.get(i).getExtension());
+        for (int i = 0; i < foods.size(); i++) {
+            fill(255, 255, 0);
+            circle(foods.get(i).getPos().getX(), foods.get(i).getPos().getY(), foods.get(i).getExtension());
         }
-        for(int i=0;i<ants.length;i++){
-            fill(255,0,0);
-            circle(ants[i].getX(),ants[i].getY(),10);
+        for (int i = 0; i < ants.length; i++) {
+            fill(255, 0, 0);
+            circle(ants[i].getX(), ants[i].getY(), 10);
         }
-        for(int i=0;i<ants.length;i++)
-            ants[i].move();
+        if(key=='r')
+            for(int i=0;i<ants.length;i++)
+                ants[i].goBack();
+        else
+            for (int i = 0; i < ants.length; i++)
+                ants[i].move();
         redraw();
     }
 
